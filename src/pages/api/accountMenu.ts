fd53169bde248071c0364616057806ca8dbc467a
yang <2300223450@qq.com>
2022-07-04 12:36:27 +0800

    Add account menu req data

---
+++ src/pages/api/accountMenu.ts
@@ -0,0 +1,36 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default function MenuOptions(
+    req: NextApiRequest,
+    res: NextApiResponse){
+    res.status(200).json({
+        data:options,
+        code:0,
+        msg:''
+    })
+}
+
+export interface MenuOptions{
+    label:string
+    icon:string
+    linkTo:string
+}
+
+
+const options:MenuOptions[] = [
+    {
+        label: 'Home',
+        icon: 'eva:home-fill',
+        linkTo: '/',
+    },
+    {
+        label: 'Profile',
+        icon: 'eva:person-fill',
+        linkTo: '#',
+    },
+    {
+        label: 'Settings',
+        icon: 'eva:settings-2-fill',
+        linkTo: '#',
+    },
+];
