b85a431ecb03ac344bc87a0d66fe37dc2941fc16
yang <2300223450@qq.com>
2022-07-11 10:10:50 +0800

    Fix app file content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,37 @@
 import * as React from 'react';
 import {ReactElement, ReactNode} from 'react';
-import ThemeProvider from '$theme/ThemeProvider'
 import {AppProps} from 'next/app';
 import {NextPage} from "next";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import theme from "$theme/theme";
+import {CacheProvider, EmotionCache} from "@emotion/react";
+import Head from "next/head";
+import createEmotionCache from "$theme/createEmotionCache";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 export type NextPageWithLayout = NextPage & {
     getLayout?: (page: ReactElement) => ReactNode
 }
 
-type AppPropsWithLayout = AppProps & {
+interface MyAppProps extends AppProps {
     Component: NextPageWithLayout
+    emotionCache?: EmotionCache;
 }
 
-export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
+export default function MyApp({Component, pageProps, emotionCache = clientSideEmotionCache}: MyAppProps) {
     const getLayout = Component.getLayout ?? ((page) => page)
     return (
-        <ThemeProvider>
-            {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline/>
+                {getLayout(<Component {...pageProps} />)}
+            </ThemeProvider>
+        </CacheProvider>
     )
 }
